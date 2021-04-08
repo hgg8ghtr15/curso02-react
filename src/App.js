8d8df0dfe9724b2950564bf33779f16936bf4978
fabio <fabiolukascj@gmail.com>
2021-04-08 09:54:47 -0400

    Aula 13 - State, Arrow function, class fields, Destruct

--- src/App.js
+++ src/App.js
@@ -4,12 +4,54 @@ import { Component } from "react"
 
 //Component de Class-state
 class App extends Component {
+  //forma antiga de fazer State
+  // constructor(props)
+  // super(props)
+  // this.handleClique = this.handleClique.bind(this)
+
+  //Forma Moderna de fazer State - Destruct
+  state = {
+    nome: "Fabio Lucas Marconi",
+    contador: 0
+  }
+
+  //arrow function "handle" = Lidar com
+  handleClique = () => {
+    // alert("Foi clicado")
+    this.setState({ nome: prompt('Digite seu nome.') })
+  }
+
+  handleAClique = (event) => {
+    event.preventDefault();
+    let { contador } = this.state
+    this.setState({ contador: contador + 1 })
+  }
+
   render() {
+    //forma antiga de fazer State
+    // let nome2 = this.state.nome;
+
+    //Forma Moderna de fazer State
+    let { nome, contador } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <p>Bem Vindo ao React - Fábio lucas Marconi</p>
+
+          <p onClick={this.handleClique}>Clique Aqui Para mudar o <strong>Nome</strong></p>
+          <p>Bem Vindo ao React - {nome}</p>
           <img src={logo} className="App-logo" alt="Logo"></img>
+          <a
+            onClick={this.handleAClique}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Clique aqui para iniciar o contador.
+          </a>
+          <p>{contador}</p>
+
         </header>
       </div>
     )
