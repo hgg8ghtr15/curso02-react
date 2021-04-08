d2d9840daadfe6ace699857044a2217ac8cd3026
fabio <fabiolukascj@gmail.com>
2021-04-08 17:12:15 -0400

    Aula 15 - Ciclo de vida dos componentes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { Component } from "react"
 
@@ -6,34 +6,71 @@ import { Component } from "react"
 class App extends Component {
 
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'Hello',
-        body: 'Testo Aqui'
-      },
-      {
-        id: 2,
-        title: 'Hello',
-        body: 'Testo Aqui'
-      }, {
-        id: 3,
-        title: 'Hello',
-        body: 'Testo Aqui'
-      }
-    ],
+    posts: [],
+    contador: 0,
+  }
+  TimeOut = null
+
+  // Antes do render
+  componentDidMount() {
+    this.handlepopular()
+    this.handleTimeout()
+  }
+
+  //Apos render
+  componentDidUpdate() {
+    this.handleTimeout()
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.TimeOut)
+  }
+
+  // metodos criados por min arrow funciones sem this
+  handlepopular = () => {
+    setTimeout(() => {
+      this.setState(
+        {
+          posts: [
+            {
+              id: 1,
+              title: 'Hello',
+              body: 'Testo Aqui'
+            },
+            {
+              id: 2,
+              title: 'Hello',
+              body: 'Testo Aqui'
+            }, {
+              id: 3,
+              title: 'Hello',
+              body: 'Testo Aqui'
+            }
+          ]
+        }
+      )
+    }, 1000)
+  }
+
+  handleTimeout = () => {
+    let { contador } = this.state
+    this.limparTimeOut = setTimeout(() => {
+      this.setState({ contador: contador + 1 })
+    },5000)
+    
   }
 
   render() {
 
-    let { posts } = this.state;
+    let { posts, contador } = this.state;
 
     return (
       <div className="App">
+        <p>Contador: {contador}</p>
         {posts.map(
           post => (
             <div key={post.id}>
-              <h1 >{post.title}</h1>,
+              <h1>{post.title}</h1>
               <p>{post.body}</p>
             </div>
           )
